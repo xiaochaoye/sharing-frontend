089c8e66e9375883e12082caf68c642866b53aba
xiaochaoye <2861184979@qq.com>
2024-03-16 08:06:31 +0800

    完善了些用户个人主页和用户管理界面

--- src/views/user/PersonHomePage.vue
+++ src/views/user/PersonHomePage.vue
@@ -1,11 +1,163 @@
 <template>
+    <div class="person_home_content">
+        <a-card hoverable style="height: 150px; box-shadow: 0px 2px 17px -1px #c4c4c4;">
+            <a-card-meta title="测试超" description="看来我没有描述">
+                <template #avatar>
+                    <a-avatar src="https://www.keaitupian.cn/cjpic/frombd/0/253/3498215567/1247959937.jpg" :size="64" />
+                </template>
+            </a-card-meta>
+            <template #actions>
+                <a-button type="primary">编辑个人信息</a-button>
+            </template>
+        </a-card>
+        <br />
+        <a-card hoverable class="outer_card" :tab-list="tabListNoTitle" :active-tab-key="noTitleKey"
+            @tabChange="key => onTabChange(key, 'noTitleKey')">
+            <a-card v-if="noTitleKey === 'mine'" v-for="card in minecards" :key="card.id" hoverable>
+                <a-card-meta :title="card.title" :description="card.description">
+                    <template #avatar>
+                        <a-avatar :size="64">{{ card.author.charAt(0) }}</a-avatar>
+                    </template>
+                </a-card-meta>
+            </a-card>
+            <a-card v-else v-for="collect in cardsCollection" :key="collect.id" hoverable>
+                <a-card-meta :title="collect.title" :description="collect.description">
+                    <template #avatar>
+                        <a-avatar :size="64">{{ collect.author.charAt(0) }}</a-avatar>
+                    </template>
+                </a-card-meta>
+            </a-card> 
+        </a-card>
+    </div>
 
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
 
+const tabListNoTitle = [
+    {
+        key: 'mine',
+        tab: '我的',
+    },
+    {
+        key: 'collections',
+        tab: '收藏',
+    },
+];
+
+const noTitleKey = ref('app')
+const onTabChange = (value: string, type: string) => {
+    console.log(value, type);
+    if (type === 'key') {
+        key.value = value;
+    } else if (type === 'noTitleKey') {
+        noTitleKey.value = value;
+    }
+};
+
+const minecards = ref([
+    {
+        id: 1,
+        title: "第三篇文章",
+        description: "无需多言3",
+        author: "陈俊渝",
+        cover: "https://aliyuncdn.antdv.com/logo.png",
+        isDisabled: false,
+        clickCount: 0,
+        likeCount: 0
+    },
+    {
+        id: 2,
+        title: "第四篇文章",
+        description: "无需多言4",
+        author: "杨曜伟",
+        cover: "https://avatars.githubusercontent.com/u/31885971?v=4",
+        isDisabled: false,
+        clickCount: 0,
+        likeCount: 0
+    },
+    {
+        id: 3,
+        title: "第五篇文章",
+        description: "无需多言5",
+        author: "陈国汉",
+        cover: "https://avatars.githubusercontent.com/u/31885971?v=4",
+        isDisabled: false,
+        clickCount: 0,
+        likeCount: 0
+    },
+    {
+        id: 4,
+        title: "第六篇文章",
+        description: "无需多言6",
+        author: "陈国汉",
+        cover: "https://avatars.githubusercontent.com/u/31885971?v=4",
+        isDisabled: false,
+        clickCount: 0,
+        likeCount: 0
+    },
+]);
+const cardsCollection = ref([
+    {
+        id: 1,
+        title: "收藏文章1",
+        description: "无需多言3",
+        author: "陈俊渝",
+        cover: "https://aliyuncdn.antdv.com/logo.png",
+        isDisabled: false,
+        clickCount: 0,
+        likeCount: 0
+    },
+    {
+        id: 2,
+        title: "收藏文章2",
+        description: "无需多言4",
+        author: "杨曜伟",
+        cover: "https://avatars.githubusercontent.com/u/31885971?v=4",
+        isDisabled: false,
+        clickCount: 0,
+        likeCount: 0
+    },
+    {
+        id: 3,
+        title: "收藏文章3",
+        description: "无需多言5",
+        author: "陈国汉",
+        cover: "https://avatars.githubusercontent.com/u/31885971?v=4",
+        isDisabled: false,
+        clickCount: 0,
+        likeCount: 0
+    },
+    {
+        id: 4,
+        title: "第六篇文章",
+        description: "无需多言6",
+        author: "陈国汉",
+        cover: "https://avatars.githubusercontent.com/u/31885971?v=4",
+        isDisabled: false,
+        clickCount: 0,
+        likeCount: 0
+    },
+]);
 </script>
 
 <style scoped lang="less">
+.person_home_content {
+    margin: 30px auto 0;
+    background-color: #cdcbcbb0;
+    height: calc(100% - 50px);
+    padding: 20px;
+    box-sizing: border-box;
+    max-width: 1000px;
+    min-width: 700px;
+    opacity: 0.9;
+}
 
+.outer_card {
+    width: 100%;
+    box-shadow: 0px 2px 17px -1px #c4c4c4;
+    overflow: auto;
+    height: 360px
+}
 </style>
--- src/views/user/UserManage.vue
+++ src/views/user/UserManage.vue
@@ -3,18 +3,17 @@
     <div class="tag-content">
       <div class="left-content">
         <div class="itemCon">
-          <span class="label">入库时间：</span>
-          <el-input v-model="inputVal" style="width: 240px" placeholder="Please input"/>
+          <span class="label">用户昵称：</span>
+          <el-input v-model="inputVal" style="width: 240px" placeholder="Please input" />
         </div>
         <div class="itemCon">
-          <span class="label">入库时间：</span>
-          <el-input v-model="inputVal" style="width: 240px" placeholder="Please input"/>
+          <span class="label">用户ID：</span>
+          <el-input v-model="inputVal" style="width: 240px" placeholder="Please input" />
         </div>
         <div class="itemCon">
-          <span class="label">入库时间：</span>
-          <el-input v-model="inputVal" style="width: 240px" placeholder="Please input"/>
+          <span class="label">性别：</span>
+          <el-input v-model="inputVal" style="width: 240px" placeholder="Please input" />
         </div>
-
       </div>
       <div class="right-content">
         <el-button type="primary" @click="handleSearch">查 询</el-button>
@@ -23,16 +22,17 @@
     </div>
 
     <div class="tableContent">
-      <el-table :data="tableData" style="width: 100%">
-        <el-table-column fixed prop="date" label="用户昵称" width="150"/>
-        <el-table-column prop="name" label="用户ID" width="120"/>
-        <el-table-column prop="state" label="头像" width="120"/>
-        <el-table-column prop="city" label="手机号" width="120"/>
-        <el-table-column prop="address" label="邮箱" width="600"/>
-        <el-table-column prop="zip" label="性别" width="120"/>
-        <el-table-column prop="zip" label="是否删除" width="120"/>
-        <el-table-column prop="zip" label="用户角色" width="120"/>
-        <el-table-column fixed="right" label="生日" width="120">
+      <el-table :data="tableData" style="width: 100%; overflow: auto;" show-overflow-tooltip="true">
+        <el-table-column fixed prop="username" label="用户昵称" width="150" />
+        <el-table-column prop="id" label="用户ID" width="120" />
+        <el-table-column prop="avatarUrl" label="头像" width="120" />
+        <el-table-column prop="phone" label="手机号" width="120" />
+        <el-table-column prop="email" label="邮箱" width="120" />
+        <el-table-column prop="gender" label="性别" width="120" :formatter="genderFormate" />
+        <el-table-column prop="isDelete" label="是否删除" width="120" />
+        <el-table-column prop="userRole" label="用户角色" width="120" />
+        <el-table-column prop="userBirthday" label="生日" width="120" />
+        <el-table-column fixed="right" prop label="操作" width="120">
           <template #default>
             <el-button link type="primary" size="small" @click="handleClick">Detail</el-button>
             <el-button link type="primary" size="small">Edit</el-button>
@@ -45,8 +45,12 @@
 </template>
 
 <script setup lang="ts">
-import {ref} from "vue";
-import {ElMessageBox} from "element-plus";
+import { ref } from "vue";
+import { ElMessageBox } from "element-plus";
+
+const genderormate = (row, column) => {
+  return row.gender === 0 ? "男" : "女"
+}
 
 const inputVal = ref('asfsf')
 
@@ -60,40 +64,114 @@ const handleClick = () => {
 
 const tableData = [
   {
-    date: '2016-05-03',
-    name: 'Tom',
-    state: 'California',
-    city: 'Los Angeles',
-    address: 'No. 189, Grove St, Los Angeles',
-    zip: 'CA 90036',
-    tag: 'Home',
+    "username": "嘻哈一怪",
+    "id": "1",
+    "avatarUrl": "https://pic2.zhimg.com/v2-2387649d9de0ef6b74aac0f69fbc8b59_r.jpg",
+    "phone": "17833781538",
+    "email": "2861184979@qq.com",
+    "isDelete": "0",
+    "userRole": "1",
+    "gender": 0,
+    "userBirthday": "14/3/2024"
   },
   {
-    date: '2016-05-02',
-    name: 'Tom',
-    state: 'California',
-    city: 'Los Angeles',
-    address: 'No. 189, Grove St, Los Angeles',
-    zip: 'CA 90036',
-    tag: 'Office',
+    "username": "蔡徐坤",
+    "id": "2",
+    "avatarUrl": "https://pic2.zhimg.com/v2-2387649d9de0ef6b74aac0f69fbc8b59_r.jpg",
+    "phone": "17833781538",
+    "email": "2861184979@qq.com",
+    "isDelete": "0",
+    "userRole": "0",
+    "gender": "",
+    "userBirthday": "14/3/2024"
   },
   {
-    date: '2016-05-04',
-    name: 'Tom',
-    state: 'California',
-    city: 'Los Angeles',
-    address: 'No. 189, Grove St, Los Angeles',
-    zip: 'CA 90036',
-    tag: 'Home',
+    "username": "ikun",
+    "id": "3",
+    "avatarUrl": "",
+    "phone": "17833781538",
+    "email": "2861184979@qq.com",
+    "isDelete": "0",
+    "userRole": "0",
+    "gender": "",
+    "userBirthday": ""
   },
   {
-    date: '2016-05-01',
-    name: 'Tom',
-    state: 'California',
-    city: 'Los Angeles',
-    address: 'No. 189, Grove St, Los Angeles',
-    zip: 'CA 90036',
-    tag: 'Office',
+    "username": "ijun",
+    "id": "4",
+    "avatarUrl": "",
+    "phone": "17833781538",
+    "email": "2861184979@qq.com",
+    "isDelete": "0",
+    "userRole": "0",
+    "gender": "",
+    "userBirthday": ""
+  },
+  {
+    "username": "",
+    "id": "5",
+    "avatarUrl": "",
+    "phone": "17833781538",
+    "email": "2861184979@qq.com",
+    "isDelete": "0",
+    "userRole": "0",
+    "gender": "",
+    "userBirthday": ""
+  },
+  {
+    "username": "",
+    "id": "6",
+    "avatarUrl": "",
+    "phone": "17833781538",
+    "email": "2861184979@qq.com",
+    "isDelete": "0",
+    "userRole": "0",
+    "gender": "",
+    "userBirthday": ""
+  },
+  {
+    "username": "",
+    "id": "7",
+    "avatarUrl": "",
+    "phone": "17833781538",
+    "email": "2861184979@qq.com",
+    "isDelete": "0",
+    "userRole": "0",
+    "gender": "",
+    "userBirthday": ""
+  },
+  {
+    "username": "",
+    "id": "8",
+    "avatarUrl": "",
+    "phone": "17833781538",
+    "email": "2861184979@qq.com",
+    "isDelete": "0",
+    "userRole": "0",
+    "gender": "",
+    "userBirthday": ""
+  },
+  {
+    "username": "",
+    "id": "9",
+    "avatarUrl": "",
+    "phone": "17833781538",
+    "email": "2861184979@qq.com",
+    "isDelete": "0",
+    "userRole": "0",
+    "gender": "",
+    "userBirthday": ""
+  },
+  {
+    "username": "",
+    "id": "10",
+    "avatarUrl": "",
+    "phone": "17833781538",
+    "email": "2861184979@qq.com",
+    "isDelete": "0",
+    "userRole": "0",
+    "gender": "",
+    "userBirthday": ""
   },
 ]
 </script>
@@ -133,6 +211,14 @@ const tableData = [
 
       span {
         font-size: 16px;
+        display: inline-block;
+        width: 100px;
+        text-align: right;
+        margin-right: 10px;
+      }
+
+      el-input {
+        flex: 1;
       }
     }
   }
